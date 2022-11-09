38f173390b77b1fc17288740ef9d45614f5f2562
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-11-08 21:47:12 -0700

    setup next.js project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>moving-balls</title>
+        <meta name="description" content="react-three-fiber animated balls with timeline scrubber." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
